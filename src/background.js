ce7943a3a6f59d150cdc627827ca9fbe2d080f21
Daniel Shogbon <ishogbon@gmail.com>
2024-09-19 08:06:19 +0100

    first, commit, everything dope

---
+++ src/background.js
@@ -0,0 +1,59 @@
+// Function to add the rule
+function addBlockMainFrameRule() {
+    chrome.declarativeNetRequest.updateSessionRules({
+        addRules: [{
+            'id': 1,
+            'priority': 1,
+            'action': {
+                'type': 'block',
+            },
+            'condition': {
+                'urlFilter': '*',
+                'resourceTypes': [
+                    'main_frame',
+                ]
+            }
+        }],
+        removeRuleIds: [1]
+    });
+}
+
+function createAllowTabRule(tabId) {
+    chrome.declarativeNetRequest.updateSessionRules({
+        addRules: [{
+            id: tabId,
+            priority: 2,
+            action: { type: "allow" },
+            condition: {
+                tabIds: [tabId],
+                urlFilter: "*",
+                resourceTypes: ["main_frame"]
+            }
+        }],
+        removeRuleIds: [tabId]
+    });
+}
+
+function removeAllowTabRule(tabId) {
+    chrome.declarativeNetRequest.updateSessionRules({
+        removeRuleIds: [tabId]
+    });
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+    addBlockMainFrameRule();
+});
+
+chrome.runtime.onStartup.addListener(() => {
+    addBlockMainFrameRule();
+})
+
+chrome.tabs.onCreated.addListener((tab) => {
+    setTimeout(() => {
+        createAllowTabRule(tab.id);
+    }, 3000)
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+    removeAllowTabRule(tabId);
+})
